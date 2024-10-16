d749d333f6fb0628b17d58d76ed0b7ca444eeb82
Moorer0523 <114755574+Moorer0523@users.noreply.github.com>
2024-10-16 19:40:28 -0400

    Delete src/environments/environment.development.ts

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    apiUrl: 'https://localhost:7190/api',
-    auth0Domain: 'dev-2lj715snuhzz1p1e.us.auth0.com',
-    auth0ClientId: 'vXYPnkwoB8pmFoBaX8UaX6eilPsuRTJL',
-    auth0Audience: '',
-}

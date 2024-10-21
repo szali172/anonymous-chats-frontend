f9b4c748b22fb2e7908d1a03a683d3d801eabb83
Rob Moore <moorer0523@gmail.com>
2024-10-21 16:05:21 -0400

    added some styling

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -2,12 +2,14 @@ import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,MatCardModule, MatButtonModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
